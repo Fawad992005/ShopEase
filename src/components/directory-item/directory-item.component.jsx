42d80c5562a8a079a48563168fcf376e41e5b344
Fawad Ahmed <fawaadahmed514@gmail.com>
2024-12-11 12:43:59 +0500

    Changes made

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,11 +1,16 @@
 import "./directory-item.styles.scss";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Directoryitem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+  const NavigationHandler = () => {
+    navigate(route);
+  };
   return (
     <>
-      <div className="directory-item-container">
+      <div className="directory-item-container" onClick={NavigationHandler}>
         <div
           className="background-image"
           style={{
