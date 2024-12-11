e67887ce21bedcbb19f5a4b8415a66a365a5a3c4
Fawad Ahmed <fawaadahmed514@gmail.com>
2024-12-11 13:24:15 +0500

    added redirects

--- src/context/categories.context.jsx
+++ src/context/categories.context.jsx
@@ -12,7 +12,7 @@ export const CategoriesProvider = ({ children }) => {
   useEffect(() => {
     const getCategoriesmap = async () => {
       const categoryMap = await getCategoriesAndDocuments();
-      console.log(categoryMap);
+
       setcategoriesMap(categoryMap);
     };
 
